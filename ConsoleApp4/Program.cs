a25244451ed4a12db7e47a79280375723a51fe39
hirnips <hdave3180@gmail.com>
2017-11-29 20:07:30 -0500

    THIS IS SECOND CHANGES

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApp4
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("THIS IS SECOND TESTING");
+            Console.WriteLine("HI THIS IS ANOTHER LINE");
         }
     }
 }
